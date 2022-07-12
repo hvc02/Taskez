4f245e216624f35aba52af3f4504241dfc7334c2
harivansh <harivansh@instoried.com>
2022-07-12 23:05:04 +0530

    BE: Login, Signup validation functions added

---
+++ validation/register.js
@@ -0,0 +1,14 @@
+const Joi = require("@hapi/joi");
+
+// Register validation
+const registerValidation = (data) => {
+  const schema = Joi.object({
+    name: Joi.string().required().label("Name"),
+    email: Joi.string().required().email().label("Email"),
+    password: Joi.string().min(8).required().label("Password"),
+  });
+
+  return schema.validate(data);
+};
+
+module.exports = registerValidation;
