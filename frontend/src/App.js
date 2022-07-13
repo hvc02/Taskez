5fb013ab15c5d4a9bda41d7e7df6568c21e6d9b1
harivansh <harivansh@instoried.com>
2022-07-13 19:30:23 +0530

    FE: Routes, components updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,40 @@
 import React from "react";
+import Auth from "./pages/Auth";
+import Projects from "./pages/Projects";
+import Spinner from "./components/Spinner/Spinner";
+import { Route, Routes, Navigate } from "react-router-dom";
+import useFindUser from "./hooks/useFindUser";
+import { UserContext } from "./context/user.context";
+import ProtectedRoute from "./ProtectedRoute";
 
 const App = () => {
-  return <div>Hello world!</div>;
+  const { user, setUser, isLoading } = useFindUser();
+
+  if (isLoading)
+    return (
+      <div className="app__loader">
+        <Spinner />
+      </div>
+    );
+  return (
+    <div className="app">
+      <UserContext.Provider value={{ user, setUser, isLoading }}>
+        <Routes>
+          <Route
+            exact
+            path="/projects"
+            element={
+              <ProtectedRoute>
+                <Projects />
+              </ProtectedRoute>
+            }
+          />
+          <Route exact path="/" element={<Auth />} />
+          <Route exact path="*" element={<Navigate replace to="/" />} />
+        </Routes>
+      </UserContext.Provider>
+    </div>
+  );
 };
 
 export default App;
