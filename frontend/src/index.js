cbaefbe2c623825e2511eb573df7dafcf847f090
harivansh <harivansh@instoried.com>
2022-07-13 19:30:51 +0530

    FE: React router, cookies provider added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/css/main.css";
-import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
+import { CookiesProvider } from "react-cookie";
+import App from "./App";
+import "./styles/css/main.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Router>
+    <CookiesProvider>
+      <App />
+    </CookiesProvider>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
