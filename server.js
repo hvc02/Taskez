fb09f22696fda647d94089d0791d45f38a35f8ef
harivansh <harivansh@instoried.com>
2022-07-13 19:30:07 +0530

    BE: Express main server.js file added

---
+++ server.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const user = require("./routes/user");
+const auth = require("./middleware/auth");
+
+// App config
+const app = express();
+const port = process.env.SERVER_PORT;
+
+// Database connection
+require("./utils/database").connect();
+
+// Middleware
+app.use(express.json());
+app.use(cors({ credentials: true }));
+
+app.use("/api/user", user);
+
+//Routes
+app.get("/welcome", auth, (req, res) => {
+  res.status(200).send("Welcome 🙌 ");
+});
+
+// Listener
+app.listen(port, () => console.log(`Listening on port: ${port}`));
