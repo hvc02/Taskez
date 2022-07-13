1f482f6b3301f9941fede45addf0ba3e930c4acc
harivansh <harivansh@instoried.com>
2022-07-13 19:22:21 +0530

    FE: store added

---
+++ frontend/src/context/store.js
@@ -0,0 +1,22 @@
+const data = {
+  boards: {
+    "board-1": {
+      id: "board-1",
+      title: "To do",
+      cards: [],
+    },
+    "board-2": {
+      id: "board-2",
+      title: "In Progress",
+      cards: [],
+    },
+    "board-3": {
+      id: "board-3",
+      title: "Completed",
+      cards: [],
+    },
+  },
+  boardIds: ["board-1", "board-2", "board-3"],
+};
+
+export default data;
